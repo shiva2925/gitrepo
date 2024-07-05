ceae79328b60385d5794b65b69580a915a69b23b
shiva2925 <shivavisualpath29@gmail.com>
2024-07-05 18:37:04 +0530

    address added

--- data.js
+++ data.js
@@ -2,5 +2,10 @@ const userdata={
     name: 'John Doe',
     age: 30,
     email: 'johndoe@example.com',
- 
+    address: {
+        street: '123 Main St',
+        city: 'New York',
+        state: 'NY',
+        zip: '12345'
+    },
 }
